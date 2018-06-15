efd0b848258762ae346228f4b4aac7e374977cd6
adamjonas <jonas@flatironschool.com>
2018-06-14 21:04:42 -0400

    initial commit

---
+++ test/index_test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from ipynb.fs.full.index import *
+
+class FieldElementTest(TestCase):
+
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        self.assertEqual(a+b, FieldElement(17, 31))
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a+b, FieldElement(7, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a-b, FieldElement(25, 31))
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a-b, FieldElement(16, 31))
